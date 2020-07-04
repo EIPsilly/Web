022947848ec59d2c76300c3db7013be9b357b443
shk314159 <296155457@qq.com>
2020-07-04 10:32:44 +0800

    dao类划分，增加打卡功能，删除更改理解有问题？讨论

--- src/com/control/QRCodeServlet.java
+++ src/com/control/QRCodeServlet.java
@@ -15,19 +15,21 @@ import java.util.Date;
 
 //import static model.QRCode.QREncode;
 
-@WebServlet(name = "QRCodeServlet", urlPatterns = "/qrCode.do")
+@WebServlet(name = "QRCodeServlet", urlPatterns = "/QRCode.do")
 public class QRCodeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
         String name = request.getParameter("name");
         String IDcard = request.getParameter("IDcard");
         String Sno = request.getParameter("Sno");
         String Phone = request.getParameter("phone");
-        String Leave = request.getParameter("Leave");
-        String Abroad = request.getParameter("abroad");
-        String Contact = request.getParameter("contact");
-        String Diagnosis = request.getParameter("diagnosis");
-        String[] HealthyStatus = request.getParameterValues("HealthStatus");
+        String Leave = request.getParameter("Leave");//是否去过疫区
+        String Abroad = request.getParameter("abroad");//是否去过国外
+        String Contact = request.getParameter("contact");//是否接触
+        String Diagnosis = request.getParameter("diagnosis");//确诊
+        String[] HealthyStatus = request.getParameterValues("HealthStatus");//状况
 
         int color = 0xFF008000;
         if (Contact.equals("Yes") || (HealthyStatus != null && HealthyStatus.length >= 2) || Diagnosis.equals("Yes")) color = 0xFFFF0000;
@@ -39,7 +41,6 @@ public class QRCodeServlet extends HttpServlet {
         File outputfile = new File(path+filename);
 //        ImageIO.write(code,"jpeg",outputfile);
         System.out.println(filename);
-
         HttpSession session = request.getSession();
         session.setAttribute("code",filename);
         session.setAttribute("name",name);
